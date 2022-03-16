891328238de35e63f1f84f3496976f16e6cc1597
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-03-16 15:20:47 +0300

    login page
    Styling the form

--- admin/index.php
+++ admin/index.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login-Ticket Booking</title>
+    <title>Admin-Login</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
+
 </head>
+
 <body>
+     <div class="container text-center text-info">
+        <form action="" method="POST" class="login-email">
+            <p class="login-text" style="font-size: 2rem; font-weight:800;">Admin-Login</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" name="email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" name="password"
+                    required>
+            </div>
+            <div class="input-group d-flex justify-content-center">
+                <button name="submit" class="btn btn-info">Login</button>
+            </div>
+        </form>
+   
+    </div>
     
+</body>
+
+</html>
 </body>
 </html>
