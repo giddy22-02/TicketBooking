a6fdcc4b5b5e9316b72394b6e778bedf31c121cc
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-03-16 15:55:32 +0300

    superadmin login

--- super_admin/index.php
+++ super_admin/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--Page Title-->
-    <title>Admin-Login</title>
+    <title>SuperAdmin-Login</title>
     <!--Bootstrap File-->
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <!--Custom CSS file-->
@@ -19,7 +19,7 @@
     <div class="container text-center">
         <!--Form Start-->
         <form action="" method="POST" class="login-email">
-            <p class="login-text" style="font-size: 2rem; font-weight:800;">Admin-Login</p>
+            <p class="login-text" style="font-size: 2rem; font-weight:800;">Super Admin-Login</p>
             <div class="input-group">
                 <input type="email" placeholder="Email" name="email" required>
             </div>
